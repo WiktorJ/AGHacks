4de5dc84c2bdcc3c0c9e7b5430514b68280da7ce
wiktorj <Wiktor.Jurasz@gmail.com>
2015-10-24 20:25:18 +0200

    quick fix

--- src/intervals/intervals.controller.js
+++ src/intervals/intervals.controller.js
@@ -4,31 +4,6 @@
 
 
 app.controller('intervalsController', function ($scope) {
-
-    $scope.test = function() {
-                MIDI.loadPlugin({
-                soundfontUrl: "./soundfont/",
-                instrument: "acoustic_grand_piano",
-                onprogress: function(state, progress) {
-                    console.log(state, progress);
-                },
-                onsuccess: function() {
-                    var delay = 0; // play one note every quarter second
-                    var note = 50; // the MIDI note
-                    var velocity = 127; // how hard the note hits
-                    // play the note
-                    MIDI.setVolume(0, 127);
-                    MIDI.noteOn(0, note , velocity, delay);
-                    MIDI.noteOff(0, note, delay + 0.75);
-                }
-            });
-    };
-
-    $("#test1").click(function() {
-        $("#key1").click();
-        $("#key2").click();
-    });
-
     $scope.keySound = function(i) {
         MIDI.loadPlugin({
             soundfontUrl: "./soundfont/",
@@ -46,8 +21,9 @@ app.controller('intervalsController', function ($scope) {
                 MIDI.noteOff(0, note + i, delay + 0.75);
             }
         });
-    }
-});    var numberToIntervalName = {
+    };
+
+   var numberToIntervalName = {
         0: 'pryma',
         1: 'sekunda mała',
         2: 'sekunda wielka',
