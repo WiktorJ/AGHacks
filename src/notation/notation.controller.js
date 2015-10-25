63b5712b75cc9b50e3f8bf5b71c13bd5dbcbf300
Marcin Zareba <marcin.zareba1@gmail.com>
2015-10-25 05:37:11 +0100

    Notation game modified

--- src/notation/notation.controller.js
+++ src/notation/notation.controller.js
@@ -18,42 +18,43 @@ app.controller('notationController', function ($scope, $timeout) {
     };
 
     $scope.createRandomSymphony = function (nSegments) {
+        var returnSegment = [];
         var outputSegment = "";
         console.log(nSegments);
         for (var i = 0; i < nSegments; i++) {
 
-
+            outputSegment = "";
             var randomNumber = $scope.getNumbersFromRange(4, 6);
             if (randomNumber >= 6) {
                 outputSegment = outputSegment + $scope.getRandomNote();
                 var hashRandomNumber = $scope.getNumbersFromRange(0, 10);
                 outputSegment = outputSegment + '/' + randomNumber.toString();
+                outputSegment = outputSegment + ' ';
+                returnSegment[i] = outputSegment;
             }
             else {
                 var greaterRandomNumber = $scope.getNumbersFromRange(4, 10);
                 var smallerNumber = $scope.getNumbersFromRange(4, 6);
                 var modifier = ["", "n", "#", "@"];
                 outputSegment = outputSegment + $scope.getRandomNote() + modifier[$scope.getNumbersFromRange(0, modifier.length)] + '/' + smallerNumber;
+                outputSegment = outputSegment + ' ';
+                returnSegment[i] = outputSegment;
             }
 
-            outputSegment = outputSegment + ' ';
         }
-        return outputSegment;
+        return returnSegment;
     };
 
-    $scope.createAnnotations = function (nSegments) {
-        var outputSegment = "\ntext ";
-        console.log(nSegments);
-        var j;
-        for (j = 1; j < nSegments; j++) {
-            outputSegment = outputSegment + j + ",";
-        }
-        outputSegment = outputSegment + j;
-        return outputSegment;
-    };
 
-    $scope.generatedNotes = $scope.createRandomSymphony($scope.numberOfSegments);
-    $scope.randomNotes = $scope.notesOptions + $scope.generatedNotes + $scope.createAnnotations($scope.numberOfSegments);
+    var tmpNotes = $scope.createRandomSymphony($scope.numberOfSegments);
+    $scope.genNotes ="";
+    $scope.generatedNotes = [];
+    for (var i = 0; i < $scope.numberOfSegments; i++) {
+        $scope.genNotes = $scope.genNotes + tmpNotes[i] + " "
+        $scope.generatedNotes[i] = tmpNotes[i];
+    }
+    //$scope.randomNotes = $scope.notesOptions + $scope.generatedNotes + $scope.createAnnotations($scope.numberOfSegments);
+    $scope.randomNotes = $scope.notesOptions + $scope.genNotes;
     console.log("scoper: " + $scope.randomNotes);
 
 
@@ -61,35 +62,52 @@ app.controller('notationController', function ($scope, $timeout) {
     //0 -> not known
     //-1 -> false
     $scope.correctAnswer = 0;
+    $scope.correctA = [-1,-1,-1];
     $scope.check = function () {
 
+        var yourScore = 0;
         if (newNotes) {
-            $scope.generatedNotes = $scope.createRandomSymphony($scope.numberOfSegments);
-            $scope.randomNotes = $scope.notesOptions + $scope.generatedNotes + $scope.createAnnotations($scope.numberOfSegments);
-            document.getElementById('notationInput').value = "";
+            $scope.text = "Sprawdź";
+            $scope.correctAnswer = 0;
+            for (var i = 0; i < $scope.numberOfSegments; i++) {
+                var j = i + 1;
+                $scope["notationInput"+j] = "";
+                $scope.correctA[i]=-1;
+            }
+            var tmpNotes = $scope.createRandomSymphony($scope.numberOfSegments);
+            $scope.genNotes ="";
+            $scope.generatedNotes = [];
+            for (var i = 0; i < $scope.numberOfSegments; i++) {
+                $scope.genNotes = $scope.genNotes + tmpNotes[i] + " "
+                $scope.generatedNotes[i] = tmpNotes[i];
+            }
+            $scope.randomNotes = $scope.notesOptions + $scope.genNotes;
+            console.log("scoper: " + $scope.randomNotes);
             newNotes = false;
         } else {
 
+            for (var i = 0; i < $scope.numberOfSegments; i++) {
+                var j = i + 1;
+                console.log($scope["notationInput"+j]);
+                console.log($scope.generatedNotes[i]);
 
-            console.log(document.getElementById('notationInput').value);
-            console.log($scope.generatedNotes);
-
-            if (document.getElementById('notationInput').value.trim() == $scope.generatedNotes.trim()) {
-                $scope.correctAnswer = 1;
-                $scope.text = "Ok";
-                $timeout(function () {
-                    $scope.correctAnswer = 0;
-                    $scope.text = "Powtórz";
-                }, 1000);
-                newNotes = true;
+                if ($scope["notationInput"+j] == $scope.generatedNotes[i].trim()) {
+                    $scope.correctA[i] = 1;
+                    yourScore = yourScore + 1;
+                    console.log("YEAAAH");
+                }
+                else {
+                    $scope.correctA[i] = 0;
+                }
             }
-            else {
+
+            if (yourScore == $scope.numberOfSegments) {
+                $scope.correctAnswer = 1;
+                $scope.text = "Powtórz";
+                newNotes = true;
+            } else {
                 $scope.correctAnswer = -1;
                 $scope.text = "Błąd";
-                $timeout(function () {
-                    $scope.correctAnswer = 0;
-                    $scope.text = "Sprawdź";
-                }, 3000);
             }
         }
     }
